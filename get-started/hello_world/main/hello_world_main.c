5325e256751d0826ff95d5c3d4f7f3743adca829
wm <gzbjwm@gmail.com>
2021-03-20 12:08:14 +0800

    1

--- get-started/hello_world/main/hello_world_main.c
+++ get-started/hello_world/main/hello_world_main.c
@@ -28,7 +28,7 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 5; i >= 0; i--) {
+    for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
